bd44d227dd3ac86a80f15693ea5b2d53e294da12
vaebe <18137693952@163.com>
2024-07-02 22:10:24 +0800

    feat(user): dto 数据类型更新

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,16 @@
+import { Length, IsNotEmpty, IsString } from 'class-validator';
+import { Roles } from '../../roles/roles.entity';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 20)
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 20)
   password: string;
+
+  roles?: Roles[];
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,7 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { Length, IsNotEmpty, IsString } from 'class-validator';
+import { Roles } from '../../roles/roles.entity';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 20)
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 20)
   password: string;
+
+  roles?: Roles[];
 }
