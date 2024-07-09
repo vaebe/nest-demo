8ce42443ee9a72ce267c6792059c5832a5cb9a1e
vaebe <18137693952@163.com>
2024-07-09 20:14:38 +0800

    feat: 日志配置

--- src/plugins/winston.ts
+++ src/plugins/winston.ts
@@ -26,19 +26,6 @@ export function createWinstonLogger() {
           winston.format.simple(),
         ),
       }),
-      new winston.transports.DailyRotateFile({
-        level: 'info',
-        dirname: 'logs',
-        filename: 'info-%DATE%.log',
-        datePattern: 'YYYY-MM-DD-HH',
-        zippedArchive: true,
-        maxSize: '1m',
-        maxFiles: '7d',
-        format: winston.format.combine(
-          winston.format.timestamp(),
-          winston.format.simple(),
-        ),
-      }),
     ],
   });
 
